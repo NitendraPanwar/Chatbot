64bdd504f33e848b3cb1044156939e90e253623c
Nitendra Panwar <nitendrapanwar@gmail.com>
2025-03-08 07:45:33 +0000

    Initial Chatbot file

---
+++ Chatbot.py
@@ -0,0 +1,38 @@
+#pip install -qq tensorflow
+#pip install -qq transformers
+#pip install sentencepiece
+#pip install torch==2.2.2
+#pip install torchtext==0.17.2
+#pip install numpy==1.26
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Selecting the model. You will be using "facebook/blenderbot-400M-distill" in this example.
+model_name = "facebook/blenderbot-400M-distill"
+#model_name = "google/flan-t5-base"
+
+# Load the model and tokenizer
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Define the chat function
+def chat_with_bot():
+    while True:
+        # Get user input
+        input_text = input("You: ")
+
+        # Exit conditions
+        if input_text.lower() in ["quit", "exit", "bye"]:
+            print("Chatbot: Goodbye!")
+            break
+
+        # Tokenize input and generate response
+        inputs = tokenizer.encode(input_text, return_tensors="pt")
+        outputs = model.generate(inputs, max_new_tokens=150) 
+        response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+
+        # Display bot's response
+        print("Chatbot:", response)
+
+# Start chatting
+chat_with_bot()
